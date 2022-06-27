dd40996719a0cd938f7d56662afee193e87147db
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2022-06-27 11:07:24 +0200

    three

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
@@ -18,4 +20,5 @@ import { AppComponent } from './app.component';
 export class AppModule { }
 
 // importer different modules
-// demarer l'appli et les composants
+// demarer l'application
+// grille de lecture: granularité
